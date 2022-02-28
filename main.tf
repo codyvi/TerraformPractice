4c4f2cfb5d1a4df1804102f6e2aa4b0c63a873ae
covyvi <davidcantumartinez@gmail.com>
2022-02-28 12:44:42 -0600

    vpc and subnet resources

--- main.tf
+++ main.tf
@@ -11,12 +11,32 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "my-first-server" {
-  ami = "ami-0e472ba40eb589f49"
 
-  instance_type = "t2.micro"
+# Resource to create ubuntu instance, uncomment and use terraform apply to create it
+# resource "aws_instance" "my-first-server" {
+#   ami = "ami-0e472ba40eb589f49"
+
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "Ubuntu"
+#   }
+# }
+
+# Resource to create a vpc instance, use terraform apply to crete it
+resource "aws_vpc" "first-vpc" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "Ubuntu"
+    Name = "production"
+  }
+}
+# Resource to create a subnet instance that references the vpc instance we declared above, use terraform apply to crete it
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "prod-subnet"
   }
 }
